b9d7dfbe2b5f9187401046ef0bb5b422a83e1f46
Hethini Ribeiro <hethini.ribeiro@gmail.com>
2023-01-04 14:39:42 -0200

    Operadores

---
+++ string.js
@@ -0,0 +1,9 @@
+const nome = "Ju";
+const idade = 2023-1981;
+const cidade = "São Paulo";
+const apresentacao = "Meu nome é "+nome+ ", da cidade " + cidade + " com idade "+ idade;
+
+console.log(apresentacao);
+
+const apresentacao2 =  `Meu nome é ${nome}, da cidade ${cidade} com idade ${idade}`;
+console.log(apresentacao2);
